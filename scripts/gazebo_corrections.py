393bfdddadf7ea73536b86907a1d9c5e3c418888
Eric Fang <efang@andrew.cmu.edu>
2016-11-21 01:48:20 -0500

    finished gazebo corrections script

--- scripts/gazebo_corrections.py
+++ scripts/gazebo_corrections.py
@@ -1,23 +1,39 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import Range
+from std_msgs.msg import Header
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+class GazeboCorrections(object):
+
+    def __init__(self):
+        rospy.init_node('gazebo_listener')
+        self.pub_scan = rospy.Publisher('/rplidar_synced/scan', LaserScan, queue_size=10)
+        self.pub_teraranger = rospy.Publisher('/terarangerone', Range, queue_size=10)
+        rospy.Subscriber("/rplidar/scan", LaserScan, self.callback_scan)
+        rospy.Subscriber("/teraranger", LaserScan, self.callback_teraranger)
+        rospy.spin()
+
+
+    def callback_scan(self, data):
+        msg = data
+        msg.header.frame_id = "laser_link"
+        msg.header.stamp = rospy.get_rostime()
+        self.pub_scan.publish(data)
+
+    def callback_teraranger(self, data):
+        msg = Range()
+        msg.header = Header()
+        msg.header.seq = data.header.seq
+        msg.header.frame_id = "base_link"
+        msg.header.stamp = rospy.get_rostime()
+        msg.radiation_type = Range.INFRARED
+        msg.field_of_view = 0.0523598776
+        msg.min_range = data.range_min
+        msg.max_range = data.range_max
+        msg.range = data.ranges[0]
+        self.pub_teraranger.publish(msg)
     
-def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber("chatter", String, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-
 if __name__ == '__main__':
-    listener()
+    gc = GazeboCorrections()
