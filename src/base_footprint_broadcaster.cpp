3f551a2a794d7587e0128d499c0a480d13c3f10a
eric1221bday <efang@andrew.cmu.edu>
2017-06-14 02:07:43 -0400

    changed base_footprint_broadcaster to accommodate cartographer

--- src/base_footprint_broadcaster.cpp
+++ src/base_footprint_broadcaster.cpp
@@ -1,37 +1,25 @@
 #include <ros/ros.h>
-#include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
 #include <tf2/impl/utils.h>
-#include <geometry_msgs/TransformStamped.h>
 #include <sensor_msgs/Range.h>
+#include <sensor_msgs/Imu.h>
 
-void teraranger_cb(const sensor_msgs::Range::ConstPtr& msg){
-    static tf2_ros::Buffer tfBuffer;
-    static tf2_ros::TransformListener tfListener(tfBuffer);
+void callback(const sensor_msgs::RangeConstPtr& range, const sensor_msgs::ImuConstPtr& imu){
     static tf2_ros::TransformBroadcaster br;
-
-    geometry_msgs::TransformStamped transformStamped;
-    try{
-        transformStamped = tfBuffer.lookupTransform("base_stabilized", "base_range",
-                                                    ros::Time(0));
-    }
-    catch (tf2::TransformException &ex) {
-        ROS_WARN("%s",ex.what());
-        return;
-    }
 
     double roll, pitch, yaw;
     tf2::impl::getEulerYPR(
-            tf2::impl::toQuaternion(transformStamped.transform.rotation), yaw, pitch, roll);
+            tf2::impl::toQuaternion(imu->orientation), yaw, pitch, roll);
 
     geometry_msgs::TransformStamped footprintTransform;
     footprintTransform.header.stamp = ros::Time::now();
-    footprintTransform.header.frame_id = "base_footprint";
-    footprintTransform.child_frame_id = "base_stabilized";
+    footprintTransform.header.frame_id = "map";
+    footprintTransform.child_frame_id = "map_2d";
     footprintTransform.transform.translation.x = 0.0;
     footprintTransform.transform.translation.y = 0.0;
-    footprintTransform.transform.translation.z = transformStamped.transform.translation.z +
-            msg->range * cos(roll) * cos(pitch);
+    footprintTransform.transform.translation.z = range->range * cos(roll) * cos(pitch);
     tf2::Quaternion q;
     q.setRPY(0, 0, 0);
     footprintTransform.transform.rotation.x = q.x();
@@ -47,8 +35,10 @@ int main(int argc, char** argv) {
 
     ros::NodeHandle nh;
 
-    ros::Subscriber lidar_sub = nh.subscribe<sensor_msgs::Range>
-            ("/terarangerone", 10, teraranger_cb);
+    message_filters::Subscriber<sensor_msgs::Range> teraranger_sub(nh, "terarangerone", 1);
+    message_filters::Subscriber<sensor_msgs::Imu> imu_sub(nh, "/mavros/imu/data", 1);
+    message_filters::TimeSynchronizer<sensor_msgs::Range, sensor_msgs::Imu> sync(teraranger_sub, imu_sub, 10);
+    sync.registerCallback(boost::bind(&callback, _1, _2));
 
     ros::spin();
 
