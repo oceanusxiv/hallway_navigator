1677af55331dc3092306eeb796b9a535af779dd8
eric1221bday <efang@andrew.cmu.edu>
2017-06-18 12:51:42 -0400

    fixed some eigen compilation errors

--- src/range_to_altitude_broadcaster.cpp
+++ src/range_to_altitude_broadcaster.cpp
@@ -7,7 +7,7 @@
 #include <Eigen/Geometry>
 
 geometry_msgs::TransformStamped range_transform;
-sensor_msgs::Imu orientation;
+sensor_msgs::Imu imu;
 std::string base_link, base_range, frame, child_frame;
 
 void range_callback(const sensor_msgs::RangeConstPtr& range_msg) {
@@ -23,21 +23,23 @@ void range_callback(const sensor_msgs::RangeConstPtr& range_msg) {
     rotation.w = 1;
     footprintTransform.transform.rotation = rotation;
 
-    if (orientation.orientation == geometry_msgs::Quaternion()) {
+    if (imu.orientation.x == 0 && imu.orientation.y == 0 && imu.orientation.z == 0 && imu.orientation.w == 0) {
         footprintTransform.transform.translation.z = range_msg->range;
     }
     else {
         Eigen::Vector3d range(0, 0, range_msg->range);
-        Eigen::Vector3d base_to_range(range_transform.transform.translation);
+        Eigen::Vector3d base_to_range(range_transform.transform.translation.x,
+                                      range_transform.transform.translation.y,
+                                      range_transform.transform.translation.z);
         auto base_to_ground = base_to_range + range;
         Eigen::Quaterniond q;
-        q.w() = orientation.orientation.w;
-        q.x() = orientation.orientation.x;
-        q.y() = orientation.orientation.y;
-        q.z() = orientation.orientation.z;
+        q.w() = imu.orientation.w;
+        q.x() = imu.orientation.x;
+        q.y() = imu.orientation.y;
+        q.z() = imu.orientation.z;
         auto rotated_base_to_ground = q.toRotationMatrix() * base_to_ground;
 
-        footprintTransform.transform.translation.z = rotated_base_to_ground.z;
+        footprintTransform.transform.translation.z = rotated_base_to_ground.z();
     }
 
     br.sendTransform(footprintTransform);
@@ -53,7 +55,7 @@ void static_callback(const tf2_msgs::TFMessageConstPtr& transform_msg) {
 }
 
 void imu_callback(sensor_msgs::Imu imu_msg) {
-    orientation = imu_msg;
+    imu = imu_msg;
 }
 
 int main(int argc, char** argv) {
@@ -62,12 +64,12 @@ int main(int argc, char** argv) {
     ros::NodeHandle nh;
 
     std::string range_topic, imu_topic;
-    nh.param("range_topic",range_topic, "terarangerone");
-    nh.param("imu_topic", imu_topic, "/mavros/imu/data");
-    nh.param("base_link", base_link, "base_link");
-    nh.param("base_range", base_range, "base_range");
-    nh.param("frame", frame, "map");
-    nh.param("child_frame", child_frame, "map_3d");
+    nh.param<std::string>("range_topic",range_topic, "terarangerone");
+    nh.param<std::string>("imu_topic", imu_topic, "/mavros/imu/data");
+    nh.param<std::string>("base_link", base_link, "base_link");
+    nh.param<std::string>("base_range", base_range, "base_range");
+    nh.param<std::string>("frame", frame, "map");
+    nh.param<std::string>("child_frame", child_frame, "map_3d");
 
     ros::Subscriber static_range_transform = nh.subscribe<tf2_msgs::TFMessage>("tf_static", 10, static_callback);
     ros::Subscriber teraranger_sub = nh.subscribe<sensor_msgs::Range>(range_topic, 10, range_callback);
